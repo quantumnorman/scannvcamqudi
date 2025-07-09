8496e2e24cbf032aaa64397927aa8535d0283f66
Victoria Norman <victoria@user-17.ra.phy.private.cam.ac.uk>
2025-07-09 11:08:53 +0100

    starting to write ni daq 600x hardware files

---
+++ src/qudi/gui/aom_control/aom_control_gui.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains a Qudi gui module for AOM control of laser power
+and read-out from a photodiode.
+
+John Jarman jcj27@cam.ac.uk
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import os
+import numpy as np
+from itertools import cycle
+from qtpy import QtWidgets
+from qtpy import QtCore
+from qtpy import uic
+import pyqtgraph as pg
+import functools
+import datetime
+from PySide2 import QtCore
+
+from qudi.util.colordefs import QudiPalettePale as palette
+
+from qudi.core.connector import Connector
+from qudi.core.module import GuiBase
+
+# Decorator to catch ValueError exceptions in functions which cast UI input text to other types.
+def value_error_handler(func):
+    @functools.wraps(func)
+    def check(self,*args,**kwargs):
+        try:
+            return func(self,*args,**kwargs)
+        except ValueError:
+            self.log.warn("ValueError when converting UI input - check input values")
+    return check
+
+class AomMainWindow(QtWidgets.QMainWindow):
+
+    """ Create the Main Window based on the *.ui file. """
+
+    def __init__(self):
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'aom-control.ui')
+
+        # Load it
+        super(AomMainWindow, self).__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+
+class AomControlGui(GuiBase ):
+
+    # declare connectors
+    aomlogic = Connector(interface='AomControlLogic')
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+    def on_activate(self):
+        """ Definition and initialisation of the GUI.
+        """
+        # Create main window instance
+        self._mw = AomMainWindow()
+
+        # Create 100 sample rolling buffer for the output graph
+        self.power_buffer = np.zeros(100)
+        self.power_filtered = np.zeros(100)
+        self.time = np.arange(0,100)
+
+        # Set up graph
+        self._mw.plot.setLabel('left', 'Power', units='µW')
+        self._mw.plot.setLabel('bottom', 'Time')
+        self.plotdata = pg.PlotDataItem(pen=pg.mkPen(palette.c1, width=2))
+        self.plotdata_smoothed = pg.PlotDataItem(pen=pg.mkPen(palette.c2, width=2), symbol=None)
+        self._mw.plot.addItem(self.plotdata)
+        self._mw.plot.addItem(self.plotdata_smoothed)
+
+        # Connect GUI events
+        self._mw.output_adj.valueChanged.connect(self.output_slider_moved)
+        self._mw.output_adj.sliderPressed.connect(self.output_slider_pressed)
+        self._mw.pid_enable.stateChanged.connect(self.enable_pid)
+        self._mw.setpoint.editingFinished.connect(self.setpoint_changed)
+
+        # Connect logic events
+        self.aom_logic = self.aomlogic()
+
+        self.aom_logic.sigAomUpdated.connect(self.update)
+
+        # Set slider to appropriate range
+        v_range = self.aom_logic.volt_range
+
+        self._mw.output_adj.setMinimum(v_range[0]*10)
+        self._mw.output_adj.setMaximum(v_range[1]*10)
+
+        self.aom_logic.start_poll()
+
+    def show(self):
+        """Make window visible and put it above all other windows.
+        """
+        QtWidgets.QMainWindow.show(self._mw)
+        self._mw.activateWindow()
+        self._mw.raise_()
+
+    def on_deactivate(self):
+        """ Deactivate module
+        """
+        self.aom_logic.stop_poll()
+
+    def update(self, param_dict):
+        """
+        Callback to update interface when the AOM logic produces a sigAomUpdated.
+        @param dict param_dict: Dict of parameters to use for updating GUI.
+        """
+        try:
+            # Get photodiode voltage and power
+            voltage = float(param_dict['pd-voltage'])
+            power = float(param_dict['pd-power'])
+            power_filtered = float(param_dict['pd-power-filtered'])
+            volts = float(param_dict['aom-output'])
+
+
+            # Update readout widgets
+            self._mw.voltage_readout.setText("{:.3f}".format(voltage))
+            self._mw.power_readout.setText("{:.3f}".format(power))
+            self._mw.aom_out.setText("{:.2f}".format(volts))
+
+            # Add power to rolling buffer and update plot data
+            self.power_buffer = np.roll(self.power_buffer, -1)
+            self.power_buffer[-1] = power
+
+            self.power_filtered = np.roll(self.power_filtered, -1)
+            self.power_filtered[-1] = power_filtered
+
+            self.plotdata.setData(self.time, self.power_buffer)
+            self.plotdata_smoothed.setData(self.time, self.power_filtered)
+        except:
+            # Stop polling if any exception is raised
+            self.aom_logic.stop_poll()
+            raise
+
+    def output_slider_moved(self, val):
+        volts = val / 10
+        self.aom_logic.enable_pid(False)
+        self._mw.pid_enable.setChecked(False)
+        self.aom_logic.set_aom_volts(volts)
+
+    def output_slider_pressed(self):
+        val = self._mw.output_adj.value()
+        self.output_slider_moved(val)
+
+    def enable_pid(self, val):
+        """
+        Control Loop Enable checkbox ticked
+        """
+        if val == 0:
+            # Unchecked
+            self.aom_logic.enable_pid(False)
+        elif val == 2:
+            # Checked
+            self.aom_logic.enable_pid(True)
+
+    @value_error_handler    
+    def setpoint_changed(self):
+        """
+        Setpoint changed
+        """
+        new_setpoint = float(self._mw.setpoint.text())
+        self.aom_logic.setpoint = new_setpoint
---
+++ src/qudi/logic/aom_control_logic.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+"""
+Logic module for controlling power via AOM
+
+John Jarman jcj27@cam.ac.uk
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+import numpy as np
+from datetime import datetime
+
+from qudi.core.connector import Connector
+from qudi.core.statusvariable import StatusVar
+from qudi.util.mutex import Mutex
+from qudi.core.module import LogicBase
+from qtpy import QtCore
+
+from qudi.core.configoption import ConfigOption
+
+from nidaqmx.errors import DaqError
+
+from .pid_logic import PIDLogic
+
+
+class AomControlLogic(LogicBase):
+    """
+    Control laser power with AOM
+    """
+
+    sigAomUpdated = QtCore.Signal(dict)
+    nicard = Connector(interface='NationalInstrumentsXSeries')
+
+    # Config options
+    photodiode_channel = ConfigOption('photodiode_channel', missing='error')
+    aom_channel = ConfigOption('aom_channel', '')
+    photodiode_factor = ConfigOption('photodiode_factor', 1.0)
+    query_interval = ConfigOption('query_interval', 10)
+    ui_update_interval = ConfigOption('ui_update_interval', 100)
+    volt_range = ConfigOption('aom_volt_range', [0, 5])
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+    def on_activate(self):
+        """ Definition and initialisation of the GUI.
+        """
+        # Hardware
+        self.daqcard = self.nicard()
+
+        # Kalman filter variables
+        self.x = 0.0                 # A priori estimate of x
+        self.P = 0.1                 # A priori estimate of x error
+
+        # Kalman filter params
+        self.R = 0.1**2     # Estimate of measurement variance (volts)
+        self.Q = 5E-4       # Estimate of process variance (volts)
+
+        self.pid_enabled = False
+        self.pid = PIDLogic.PID(
+            Kp=2,
+            Ki=5,
+            Kd=0,
+            setpoint=0,
+            sample_time=None,
+            output_limits=self.volt_range
+        )
+
+        self.current_volts = 0
+        self.last_update_time = 0
+        self.power = 0
+        self.power_filtered = 0
+        self.voltage_reading = 0
+
+        self.start_poll()
+        
+
+    def on_deactivate(self):
+        """Module deactivation
+        """
+        self.stop_poll()
+        
+
+    def start_poll(self):
+        """
+        Start polling DAQ
+        """
+        self._stop_poll = False
+        QtCore.QTimer.singleShot(self.query_interval, self.update_power_reading)
+
+    def stop_poll(self):
+        """
+        Stop polling DAQ
+        """
+        self._stop_poll = True
+
+
+    def update_power_reading(self):
+        """
+        Get power reading from DAQ card.
+        """
+        if self._stop_poll == True:
+            return
+
+        try:
+            # Read voltage from photodiode
+            self.voltage_reading = np.mean(self.daqcard.analog_channel_read(self.photodiode_channel))
+
+            # Do Kalman filtering
+            prev_x = self.x
+            estimate_P = self.P + self.Q
+
+            # Estimate Kalman amplitude
+            K = estimate_P / (estimate_P + self.R)
+
+            self.x = prev_x + K * (self.voltage_reading - prev_x)
+            self.P = (1-K) * estimate_P
+
+            if self.pid_enabled:
+                control_var = self.pid(self.x)
+                self.set_aom_volts(control_var)
+        
+            if (self.last_update_time + (self.ui_update_interval - self.query_interval / 2)/1000
+                < datetime.timestamp(datetime.now())):
+                # If UI update interval has passed or will pass within the next
+                # half of a query_interval, emit sigAomUpdated.
+                
+                self.last_update_time = datetime.timestamp(datetime.now())
+
+                # Convert power to volts using factor
+                self.power = self.voltage_reading * self.photodiode_factor
+                self.power_filtered = self.x * self.photodiode_factor
+
+                output_dict = {
+                    'pd-voltage':self.voltage_reading,
+                    'pd-power':self.power,
+                    'pd-power-filtered':self.power_filtered,
+                    'aom-output':self.current_volts
+                }
+
+                self.sigAomUpdated.emit(output_dict)
+                
+            QtCore.QTimer.singleShot(self.query_interval, self.update_power_reading)
+
+        except DaqError as err:
+            # Log any DAQ errors
+            self.log.error('DAQ error {}'.format(err))
+
+    def set_aom_volts(self, volts):
+        """
+        Set AOM output to specified volts.
+        """
+        volts = float(volts)
+        if volts >= min(self.volt_range) and volts <= max(self.volt_range):
+            # Check inside acceptable voltage range
+            # Write to analogue output
+            if self.aom_channel != '':
+                self.daqcard.analog_channel_write(self.aom_channel, volts)
+                self.current_volts = volts
+                return 0
+            else:
+                self.log.warn('No AOM channel specified - no output')
+                return -1
+
+    def enable_pid(self, state=True):
+        """
+        Enable/disable PID control.
+        @param bool state: True to enable, False to disable.
+        """
+        if state:
+            self.pid.set_auto_mode(True, last_output=self.current_volts)
+            self.pid_enabled = True
+        else:
+            self.pid_enabled = False
+            self.pid.set_auto_mode(False)
+
+    @property
+    def setpoint(self):
+        return self.pid.setpoint * self.photodiode_factor
+
+    @setpoint.setter
+    def setpoint(self, val):
+        volts = val / self.photodiode_factor
+        self.pid.setpoint = volts
